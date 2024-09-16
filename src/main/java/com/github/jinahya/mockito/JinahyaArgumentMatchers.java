8aa20363a4f9b5a54f2c5f11007ae8963e9c3f3c
Jin Kwon <onacit@gmail.com>
2024-09-16 22:00:03 +0900

    wip

--- src/main/java/com/github/jinahya/mockito/JinahyaArgumentMatchers.java
+++ src/main/java/com/github/jinahya/mockito/JinahyaArgumentMatchers.java
@@ -2,7 +2,49 @@ package com.github.jinahya.mockito;
 
 import org.mockito.ArgumentMatchers;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
 public class JinahyaArgumentMatchers {
+
+    public static <T> T from(final Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate, "predicate is null");
+        return ArgumentMatchers.argThat(predicate::test);
+    }
+
+    // ------------------------------------------------------------------------------------------------------------ int
+    public static Predicate<Integer> NEGATIVE_INT_NULLABLE = v -> {
+        return v == null || v < 0;
+    };
+    public static Predicate<Integer> NEGATIVE_INT_NONNULL = v -> {
+        return v != null && v < 0;
+    };
+
+    public static int negativeInt(final boolean nullable) {
+        return ArgumentMatchers.intThat(v -> {
+            if (nullable) {
+                return v == null || v < 0;
+            } else {
+                return v != null && v < 0;
+            }
+        });
+    }
+
+    public static int negativeInt() {
+        return negativeInt(false);
+    }
+
+    public static int positiveInt() {
+        return ArgumentMatchers.intThat(v -> v != null && v > 0L);
+    }
+
+    public static int nonNegativeInt() {
+        return ArgumentMatchers.intThat(v -> v != null && v >= 0L);
+    }
+
+    public static int nonPositiveInt() {
+        return ArgumentMatchers.intThat(v -> v != null && v <= 0L);
+    }
 
     // ------------------------------------------------------------------------------------------------------------ long
     public static long negativeLong() {
